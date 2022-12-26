ffb833401d76991cc847acd68308678ef029bd40
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-12-25 18:09:48 -0600

    Numbers, paths

--- src/Program.cs
+++ src/Program.cs
@@ -6,19 +6,82 @@ namespace DTNT
 {
     class Program
     {
+        static void Foo()
+        {
+            var subtotal = 20;
+            var sales_tax = subtotal + (subtotal * 0.09);
+            var total_price = subtotal + sales_tax;
+
+            Console.WriteLine("Your total is {0}\n", total_price);
+            Console.WriteLine("Today: {0:D}, Temp: {1} degrees F\n", DateTime.Today, 25);
+
+            int dec = 2_000_000;
+            int bin = 0b_0001_1110_1000_0100_1000_0000;
+            int hex = 0x_001E_8480;
+            Console.WriteLine("{0}\n{1}\n{2}", dec, bin, hex);
+            Console.WriteLine(dec == bin && dec == hex); // True
+
+            double a = 0.1d;
+            double b = 0.2d;
+            if (a + b == 0.3d) // 0.30000000000000004
+            {
+                Console.WriteLine($"True: {a + b}");
+            }
+            else
+            {
+                Console.WriteLine($"False: {a + b}");
+            }
+
+            decimal x = 0.1m;
+            decimal y = 0.2m;
+            if (x + y == 0.3m) // 0.3
+            {
+                Console.WriteLine($"True: {x + y}");
+            }
+            else
+            {
+                Console.WriteLine($"False: {x + y}");
+            }
+        }
+        static void Numbers()
+        {
+            Console.WriteLine($"\n\nint: {sizeof(int)}-byte"); // 4-byte, 16-bit
+            Console.WriteLine($"int range:\n{int.MinValue:N0}\nto\n{int.MaxValue:N0}");
+
+            Console.WriteLine($"\n\nfloat: {sizeof(float)}-byte"); // 4-byte, 16-bit
+            Console.WriteLine($"float range:\n{float.MinValue:N0}\nto\n{float.MaxValue:N0}");
+
+            Console.WriteLine($"\n\ndouble: {sizeof(double)}-byte"); // 8-byte, 32-bit
+            Console.WriteLine($"double range:\n{double.MinValue:N0}\nto\n{double.MaxValue:N0}");
+
+            Console.WriteLine($"\n\ndecimal: {sizeof(decimal)}-byte"); // 16-byte, 64-bit
+            Console.WriteLine($"decimal range:\n{decimal.MinValue:N0}\nto\n{decimal.MaxValue:N0}");
+        }
+        static void Paths()
+        {
+            var app_path = System.AppContext.BaseDirectory;
+            var domain_path = System.AppDomain.CurrentDomain.BaseDirectory;
+            var io_path = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var io_dir_path = System.IO.Directory.GetCurrentDirectory();
+            var env_path = System.Environment.CurrentDirectory;
+            // Disable the escape of special symbols like \t with @
+            var string_path = @"C:\tv_and_movies\terminator\";
+
+            Console.WriteLine("app_path: {0}", app_path);
+            Console.WriteLine("domain_path: {0}", domain_path);
+            Console.WriteLine("io_path: {0}", io_path);
+            Console.WriteLine("io_dir_path: {0}", io_dir_path);
+            Console.WriteLine("env_path: {0}", env_path);
+            Console.WriteLine("string_path: {0}", string_path);
+        }
         static void Main(string[] args)
         {
             System.Data.DataSet data_set;
             System.Net.Http.HttpClient client;
 
-            Console.WriteLine("Hello, C#!");
-
-            var subtotal = 20;
-            var sales_tax = subtotal + (subtotal * 0.09);
-            var total_price = subtotal + sales_tax;
-
-            Console.WriteLine("Your total is {0}", total_price);
-            Console.WriteLine("Today is {0:D}, and the temperature is {1} degrees F", DateTime.Today, 25);
+            Foo();
+            Numbers();
+            Paths();
 
             // Get array of referenced assembly names
             foreach (var referenced in Assembly.GetEntryAssembly().GetReferencedAssemblies())
